1647cb38657ea6b6705ee92ef95491add569f114
ruriadityap <ruriaditya257@gmail.com>
2022-06-16 10:53:31 +0700

    deploy ke heroku

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 
 // Connect to MongoDB via Mongoose
 seeder.connect(
-  "mongodb://127.0.0.1:27017/db_dntrenttt",
+  "mongodb+srv://ruriaditya:ruri081@cluster0.uigzl.mongodb.net/db_dntrentcar?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,

9c5e201c3ec0a6d0507a7bdf781b7d936c96187a
ruriadityap <ruriaditya257@gmail.com>
2022-06-16 10:25:24 +0700

    full project dntrentcar

---
+++ middlewares/multer.js
@@ -0,0 +1,44 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+// import uuid from "uuid/v4";
+
+const storage = multer.diskStorage({
+  destination: "public/images",
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const uploadSingle = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("image");
+
+const uploadMultiple = multer({
+  storage: storage,
+  // limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).array("image");
+
+// // Check file Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const fileTypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimeType = fileTypes.test(file.mimetype);
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Errir: Images Only!!!");
+  }
+}
+
+module.exports = { uploadSingle, uploadMultiple };
